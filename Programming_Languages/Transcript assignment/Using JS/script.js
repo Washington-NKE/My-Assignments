2bbdfe78bad64889b2d7b0a78e01ce412622f329
Washington <washiem22@gmail.com>
2024-07-17 12:10:57 +0300

    Add records to array and add records to the page

--- Programming_Languages/Transcript assignment/Using JS/script.js
+++ Programming_Languages/Transcript assignment/Using JS/script.js
@@ -1,4 +1,4 @@
-const studentMarks = [{
+let studentMarks = [{
     no: 1,
     studentId: '0705',
     name: 'Washington',
@@ -19,40 +19,87 @@ const studentMarks = [{
     ccs2102: 76,
     ccs2103: 72,
     sas2101: 21
+}];
+
+
+
+//Fuction to fetch and parse the csv file
+function fetchCSV() {
+   return fetch('data.csv')
+        .then(response => response.text())
+        .then(data => {
+            return new Promise((resolve, reject) => {
+                Papa.parse(data, {
+                    header: true,
+                    dynamicTyping: true,
+                    complete: function(results) {
+                        resolve(results.data);
+                },
+                error: function(error) {
+                    reject(error);
+                }
+            });
+        });
+    });
 }
-];
     
-let grade= '';
-let html = `
-<div class = "table">
-<table>
-    <tr id = "headrow">
-        <td id = "no"> S/NO</td>
-        <td id = "id">Student ID</td>
-        <td id = "marks"> Mean </td>
-        <td id = "grade" > Grade</td>
-    </tr>
-`;
+// Function to display the CSV data
+function displayData(data) {
+    data.forEach(row => {
+       studentMarks.push(row);
+    });
+    return studentMarks;
+}
 
-studentMarks.forEach((averageMarks) => {
-    const sum = averageMarks.ccs2102 + averageMarks.ccs2103 + averageMarks.sas2101;
-     const   average =  (sum / 3).toFixed(2);
-     grade = determineGrade(average);
+fetchCSV()
+    .then(data => {
+        displayData(data);
+        console.log(studentMarks);
+        console.log(studentMarks.length);
+    
+        let grade= '';
+        let html = `
+        <div class = "table">
+        <table>
+            <tr id = "headrow">
+                <td id = "no"> S/NO</td>
+                <td id = "id">Student ID</td>
+                <td id = "marks"> Mean </td>
+                <td id = "grade" > Grade</td>
+            </tr>
+        `;
 
-   html += `
-    <tr id = "${grade}">
-        <td id = "no"> ${averageMarks.no} </td>
-        <td id = "id">${averageMarks.studentId}</td>
-        <td id = "marks"> ${average}</td>
-        <td id = "grade"> ${grade}</td>
-    </tr>
-    `
-   
-});
 
-html += `  </table>
-</div>
- `
+        function generateTable (grade,html) {
+        studentMarks.forEach((averageMarks) => {
+            const sum = averageMarks.ccs2102 + averageMarks.ccs2103 + averageMarks.sas2101;
+            const   average =  (sum / 3).toFixed(2);
+            grade = determineGrade(average);
+
+        html += `
+            <tr id = "${grade}">
+                <td id = "no"> ${averageMarks.no} </td>
+                <td id = "id">${averageMarks.studentId}</td>
+                <td id = "marks"> ${average}</td>
+                <td id = "grade"> ${grade}</td>
+            </tr>
+            `;
+        
+        });
+
+        html += `  </table>
+        </div>
+        `;
+
+        return html;
+            }
+
+
+html = generateTable(grade,html);
+
+document.querySelector('.display-js').innerHTML = html;
+})
+.catch(error => console.error('Error fetching the CSV file:', erro));
 
 
 function determineGrade(average) {
@@ -71,34 +118,3 @@ function determineGrade(average) {
     grade = determineGrade(average);
 }
  
-
-
-document.querySelector('.display-js').innerHTML = html;
-
-//Fuction to fetch and parse the csv file
-function fetchCSV() {
-    fetch('data.csv')
-        .then(response => response.text())
-        .then(data => {
-            Papa.parse(data, {
-                header: true,
-                dynamicTyping: true,
-                complete: function(results) {
-                   console.log(results.data);
-                    // Process the CSV data here
-                    displayData(results.data);
-                }
-            });
-        })
-        .catch(error => console.error('Error fetching the CSV file:', error));
-        //return results.data;
-}
-
-// Function to display the CSV data
-function displayData(data) {
-    data.forEach(row => {
-        console.log(row);
-    });
-}
-
-fetchCSV();
